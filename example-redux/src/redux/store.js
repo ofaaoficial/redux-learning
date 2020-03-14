a0282900a215a025d29b10d0eefed6008635b19f
Oscar Amado <oscarfamado@gmail.com>
2020-03-14 14:53:10 -0500

    implementacion de redux-devtools-extension para pruebas con redux

--- example-redux/src/redux/store.js
+++ example-redux/src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
 import { rootReducers } from "./reducers/rootReducers";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-export const store = createStore(rootReducers);
+export const store = createStore(rootReducers, composeWithDevTools());
